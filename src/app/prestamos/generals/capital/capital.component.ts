5cf3c79f82bf219b0eef744fdbd56c731c309387
elpolakopolania <gustav0796@hotmail.com>
2021-11-29 17:44:33 -0500

    Ajuste capital

--- src/app/prestamos/generals/capital/capital.component.ts
+++ src/app/prestamos/generals/capital/capital.component.ts
@@ -1,24 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { SolicitudService } from '../../service/solicitud.service';
+import { Solicitud } from '../../model/solicitud';
 
 @Component({
   selector: 'app-capital',
   templateUrl: './capital.component.html',
-  styleUrls: ['./capital.component.css']
+  styleUrls: ['./capital.component.css'],
 })
 export class CapitalComponent implements OnInit {
+  public pagados: Solicitud[];
   capitalBase = environment.capitalBase;
-  capitalPrestado: any = 0;
+  public totalPrestado: any = 0;
   valorPrestado: any;
-  constructor(private solicitudService: SolicitudService) {
-
-   }
+  constructor(private solicitudService: SolicitudService) {}
 
   ngOnInit(): void {
-    this.solicitudService.valorPrestadoObservable.subscribe(res => {
-      this.capitalPrestado = res
+    // Calcular el total prestado
+    this.solicitudService
+      .getValues()
+      .subscribe((data) => this.calcularTotalPrestado(data));
+    this.solicitudService.valorPrestadoObservable.subscribe((res) => {
+      this.totalPrestado = res;
     });
   }
 
+  calcularTotalPrestado(solicitudes: any) {
+    this.pagados = solicitudes;
+    this.totalPrestado = 0;
+    this.pagados.forEach(
+      (row) => (this.totalPrestado = this.totalPrestado + row.value)
+    );
+    this.solicitudService.enviarValorPrestado(this.totalPrestado);
+  }
 }
